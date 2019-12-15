900319631e8d19ff6466cd2623f06fce2c855b60
Ikramullah Latif <sky_m4n@outlook.com>
2019-12-15 08:18:29 +0800

    Make error handling

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,8 @@ const uglifyJS = require('uglify-es')
 const sass = require('sass')
 const fs = require('fs')
 const pkg = require('./package.json')
+const notify = require('gulp-notify')
+const beep = require('beepbeep')
 
 const signature =
   `/**\n * ${pkg.author.project_name}\n * (C)-${moment().format('YYYY')} ${pkg.author.name} (${pkg.author.url})\n */\n`
@@ -42,21 +44,28 @@ function compileSASS () {
   // Scan semua file dalam file sass
   fs.readdir('public/sass', (err, files) => {
     if (err) throw err
+    const g = gulp.src('public/dist/main.min.css')
 
-    files.forEach(file => {
-      // Apabila ada file yang bukan berakhiran .scss, hiraukan
-      if (!file.endsWith('.scss')) return undefined
-      // Push bentuk render sass menjadi css ke dalam array
-      cssCode.push(sass.renderSync({ file: `public/sass/${file}` }).css.toString())
-    })
+    // Handling apabila ada error
+    try {
+      files.forEach(file => {
+        // Apabila ada file yang bukan berakhiran .scss, hiraukan
+        if (!file.endsWith('.scss')) return undefined
+        // Push bentuk render sass menjadi css ke dalam array
+        cssCode.push(sass.renderSync({ file: `public/sass/${file}` }).css.toString())
+      })
 
-    // Buat file baru yang isinya CSS yang telah diuglify + tanda tangan pembuatannya
-    fs.writeFileSync('public/dist/main.min.css', signature + uglifyCSS.processString(cssCode.join('\n')))
+      // Buat file baru yang isinya CSS yang telah diuglify + tanda tangan pembuatannya
+      fs.writeFileSync('public/dist/main.min.css', signature + uglifyCSS.processString(cssCode.join('\n')))
 
-    // Stream CSSnya
-    console.log(`[${chalk.green('SimpleReload')}] SCSS berhasil dicompile!`)
-    return gulp.src('public/dist/main.min.css')
-      .pipe(browserSync.stream())
+      // Stream CSSnya
+      console.log(`[${chalk.green('SimpleReload')}] SCSS berhasil dicompile!`)
+      return g.pipe(browserSync.stream())
+    } catch (error) {
+      browserSync.notify(error.message)
+      beep(5, 1000)
+      return g.pipe(notify(error.message))
+    }
   })
 }
 
@@ -68,20 +77,26 @@ function compileJS () {
 
   fs.readdir('public/js', (err, files) => {
     if (err) throw err
+    const g = gulp.src('public/dist/main.min.js')
 
-    files.forEach(file => {
-      // Apabila ada file yang bukan berakhiran .js, hiraukan
-      if (!file.endsWith('.js')) return undefined
-      // Push isi file JS ke dalam array
-      jsCode.push(uglifyJS.minify(fs.readFileSync('public/js/' + file).toString()).code)
-    })
+    try {
+      files.forEach(file => {
+        // Apabila ada file yang bukan berakhiran .js, hiraukan
+        if (!file.endsWith('.js')) return undefined
+        // Push isi file JS ke dalam array
+        jsCode.push(uglifyJS.minify(fs.readFileSync('public/js/' + file).toString()).code)
+      })
 
-    // Buat file baru yang isinya JS yang telah diuglify + tanda tangan pembuatannya
-    fs.writeFileSync('public/dist/main.min.js', signature + uglifyJS.minify(jsCode.join(';')).code)
+      // Buat file baru yang isinya JS yang telah diuglify + tanda tangan pembuatannya
+      fs.writeFileSync('public/dist/main.min.js', signature + uglifyJS.minify(jsCode.join(';')).code)
 
-    // Stream JSnya
-    console.log(`[${chalk.green('SimpleReload')}] JS berhasil dicompile!`)
-    return gulp.src('public/dist/main.min.js')
-      .pipe(browserSync.stream())
+      // Stream JSnya
+      console.log(`[${chalk.green('SimpleReload')}] JS berhasil dicompile!`)
+      return g.pipe(browserSync.stream())
+    } catch (error) {
+      browserSync.notify(error.message)
+      beep(5, 1000)
+      return g.pipe(notify(error.message))
+    }
   })
 }
